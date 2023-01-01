16b5cac0bc927f23599f1de0b669a00c83536b85
apple <apple@apples-MacBook-Pro.local>
2023-01-01 14:50:33 +0530

    get a task by id

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -17,8 +17,21 @@ const getTasks = async (req, res) => {
     res.status(500).json({ msg: error.message });
   }
 };
+const getTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findById(id);
+    if (!task) {
+      return res.status(404).json({ msg: "Task not found" });
+    }
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
 
 module.exports = {
   createTask,
   getTasks,
+  getTask,
 };
--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -1,10 +1,14 @@
 const express = require("express");
-const { createTask, getTasks } = require("../controllers/taskController");
+const {
+  createTask,
+  getTasks,
+  getTask,
+} = require("../controllers/taskController");
 
 const router = express.Router();
 
 router.post("/api/tasks", createTask);
-
 router.get("/api/tasks", getTasks);
+router.get("/api/tasks/:id", getTask);
 
 module.exports = router;
