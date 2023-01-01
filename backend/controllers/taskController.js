e3fb3298b9991e45be10f744ce8a21506f4cf475
apple <apple@apples-MacBook-Pro.local>
2023-01-01 15:08:41 +0530

    delete a task by id

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -29,9 +29,22 @@ const getTask = async (req, res) => {
     res.status(500).json({ msg: error.message });
   }
 };
+const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findByIdAndDelete(id);
+    if (!task) {
+      return res.status(404).json({ msg: "Task not found" });
+    }
+    res.status(200).send("Task Deleted !!");
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
 
 module.exports = {
   createTask,
   getTasks,
   getTask,
+  deleteTask,
 };
--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -3,6 +3,7 @@ const {
   createTask,
   getTasks,
   getTask,
+  deleteTask,
 } = require("../controllers/taskController");
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/api/tasks", createTask);
 router.get("/api/tasks", getTasks);
 router.get("/api/tasks/:id", getTask);
+router.delete("/api/tasks/:id", deleteTask);
 
 module.exports = router;
