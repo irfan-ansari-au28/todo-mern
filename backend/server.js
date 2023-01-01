1e758e659e05180e7765a5ee7f2e2dc4598373ee
apple <apple@apples-MacBook-Pro.local>
2023-01-01 08:41:22 +0530

    setup postman

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+// ROUTE
+
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
