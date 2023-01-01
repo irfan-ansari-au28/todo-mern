388701824cd7a31277d7e94569d59ef8dc96c283
apple <apple@apples-MacBook-Pro.local>
2023-01-01 16:53:41 +0530

    upadate a task by id

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,5 +1,6 @@
 const Task = require("../models/taskModel");
 
+// Create a new task
 const createTask = async (req, res) => {
   try {
     const task = await Task.create(req.body);
@@ -9,6 +10,7 @@ const createTask = async (req, res) => {
   }
 };
 
+// Get all the tasks
 const getTasks = async (req, res) => {
   try {
     const tasks = await Task.find();
@@ -17,6 +19,8 @@ const getTasks = async (req, res) => {
     res.status(500).json({ msg: error.message });
   }
 };
+
+// Get a task
 const getTask = async (req, res) => {
   try {
     const { id } = req.params;
@@ -29,6 +33,8 @@ const getTask = async (req, res) => {
     res.status(500).json({ msg: error.message });
   }
 };
+
+// Delete a task
 const deleteTask = async (req, res) => {
   try {
     const { id } = req.params;
@@ -42,9 +48,27 @@ const deleteTask = async (req, res) => {
   }
 };
 
+// Update a task
+const updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const task = await Task.findByIdAndUpdate({ _id: id }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ msg: "Task not found" });
+    }
+    res.status(200).json(task);
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+};
+
 module.exports = {
   createTask,
   getTasks,
   getTask,
   deleteTask,
+  updateTask,
 };
--- backend/routes/taskRoute.js
+++ backend/routes/taskRoute.js
@@ -4,6 +4,7 @@ const {
   getTasks,
   getTask,
   deleteTask,
+  updateTask,
 } = require("../controllers/taskController");
 
 const router = express.Router();
@@ -12,5 +13,6 @@ router.post("/api/tasks", createTask);
 router.get("/api/tasks", getTasks);
 router.get("/api/tasks/:id", getTask);
 router.delete("/api/tasks/:id", deleteTask);
+router.put("/api/tasks/:id", updateTask);
 
 module.exports = router;
